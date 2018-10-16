f627ae83e8b193ad8dc9f1c006097f8298577826
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-10-16 18:41:57 -0300

    Created folder js with scripts.js file and added DOMContentLoaded event

---
+++ 01-JS-DOM-APIs/js/scripts.js
@@ -0,0 +1,7 @@
+(function() {
+	"use strict";
+
+	document.addEventListener('DOMContentLoaded', function(){
+		document.querySelector('.is-hidden').classList.add('not-hidden');
+	}); //DOM CONTENT LOADED
+})();
