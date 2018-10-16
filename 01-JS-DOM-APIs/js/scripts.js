6aeeb0d059c710b4b715ea2b384380c9b771334e
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-10-16 18:54:56 -0300

    Created showAlertMessage function to display an alert

--- 01-JS-DOM-APIs/js/scripts.js
+++ 01-JS-DOM-APIs/js/scripts.js
@@ -3,5 +3,11 @@
 
 	document.addEventListener('DOMContentLoaded', function(){
 		document.querySelector('.is-hidden').classList.add('not-hidden');
+
+		function showAlertMessage(){
+			alert('This is an alert message!');
+		}
+
+		showAlertMessage();
 	}); //DOM CONTENT LOADED
 })();
