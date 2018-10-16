43e5b5b5befd749dbfd8187e1738523016f880bf
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-10-16 19:38:34 -0300

    Added click event to button

--- 01-JS-DOM-APIs/js/scripts.js
+++ 01-JS-DOM-APIs/js/scripts.js
@@ -2,12 +2,22 @@
 	"use strict";
 
 	document.addEventListener('DOMContentLoaded', function(){
+		
+		//Fade in section
 		document.querySelector('.is-hidden').classList.add('not-hidden');
 
+		//Function to display an alert
 		function showAlertMessage(){
 			alert('This is an alert message!');
 		}
 
 		showAlertMessage();
+
+		//Displays an alert with button is clicked
+		let btnClickMe = document.querySelector('#click-me');
+		btnClickMe.addEventListener('click', function() {
+			showAlertMessage();
+		});
+
 	}); //DOM CONTENT LOADED
 })();
