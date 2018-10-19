f34d9f61dd23bc7a1f01c02a28cbaca6e72fda27
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-10-19 17:29:58 -0300

    Created reusable function with promise and if network fails content turns red

--- 01-JS-DOM-APIs/js/scripts.js
+++ 01-JS-DOM-APIs/js/scripts.js
@@ -5,6 +5,8 @@
 
 	document.addEventListener('DOMContentLoaded', function() {
 		
+		let jokesSection = document.querySelector('#jokes');
+
 		//Hidden section with fade in effect
 		document.querySelector('.hidden-section').classList.add('fadein');
 
@@ -13,25 +15,49 @@
 			alert('This is an alert message!');
 		}
 
-		//Displays an alert when button is clicked
-		let btnClickMe = document.querySelector('#click-me');
-		btnClickMe.addEventListener('click', loadContent);
+		//Reusable function to load content from Rest Api
+		function loadContent(config) {
+			return new Promise(function(resolve, reject) {
+				let xhr = new XMLHttpRequest();
+				xhr.open(config.method, config.url, true);
+				
+				xhr.onload = function() {
+					if(xhr.status === 200) {
+						resolve(JSON.parse(xhr.response));
+					} else {
+						reject(Error('Could not load data succesfully, error code: ' + xhr.statusText));
+					}
+				};
+				
+				xhr.onerror = function (){
+					reject(Error('There was a network error'));
+				};
 
-		//Function to load content from Rest Api
-		function loadContent() {
-			let xhr = new XMLHttpRequest();
+				xhr.send();
+			});
+		}
 
-			xhr.open('GET', 'http://api.icndb.com/jokes/random', true);
-			xhr.onreadystatechange = function () {
-				if(xhr.readyState === 4 && xhr.status === 200){
-					let data = JSON.parse(xhr.responseText);
-					let html = `ID: ${data.value.id} <br> JOKE: ${data.value.joke}`;
-					document.querySelector('#jokes').innerHTML = html;
-				}
+		//Function to load jokes from Rest Api: http://api.icndb.com/jokes/random
+		function loadJokes(){
+			let config = {
+				method : 'GET',
+				url : 'http://api.icndb.com/jokes/random'
 			}
 
-			xhr.send();
+			loadContent(config)
+				.then(function(data) {
+					let html = `ID: ${data.value.id} <br> JOKE: ${data.value.joke}`;
+					document.querySelector('#jokes').innerHTML = html;
+				})
+				.catch(function(error){
+					jokesSection.classList.add('red-content');
+					jokesSection.innerHTML = error;
+				});
 		}
+
+		//Displays jokes (rest api content) when button is clicked
+		let btnClickMe = document.querySelector('#click-me');
+		btnClickMe.addEventListener('click', loadJokes);
 
 	}); //DOM CONTENT LOADED
 })();
