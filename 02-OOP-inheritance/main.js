7506449088d82e2f3a760a72e47e0581160362b3
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-10-27 14:52:00 -0300

    Ej1: Created class EventEmitter

--- 02-OOP-inheritance/main.js
+++ 02-OOP-inheritance/main.js
@@ -1,5 +1,35 @@
 /* jshint esversion: 6 */
 
+class EventEmitter {
+	constructor() {
+
+	}
+
+	on(video, eventName, callback) {
+		video.addEventListener(eventName, callback);
+	}
+
+	emit(eventName, video) {
+		switch(eventName){
+			case 'play': 
+				this.on(video, 'play', () => console.log('Playing movie') );
+				break;
+			case 'pause':
+				this.on(video, 'pause', () => console.log('Movie has been paused') );
+				break;
+			case 'resume':
+				this.on(video, 'resume', () => console.log('Movie has been resumed') );
+				break;
+			default:
+				console.log('No event has been emitted');
+		}
+	}
+
+	off(video, eventName, callback) {
+		video.removeEventListener(eventName, callback);
+	}
+}
+
 class Actor {
 	constructor(name, age) {
 		this.name = name;
@@ -8,10 +38,11 @@ class Actor {
 }
 
 class Movie {
-	constructor(name, year, duration) {
+	constructor(name, year, duration, video) {
 		this.title = name;
 		this.year = year;
 		this.duration = duration;
+		this.video = video;
 	}
 
 	play() {
@@ -27,5 +58,6 @@ class Movie {
 	}
 }
 
-const movie1 = new Movie('The Maze Runner', 2014, 113);
+let exampleVid = document.getElementById('dub-video');
+const movie1 = new Movie('The Maze Runner', 2014, 113, exampleVid);
 console.log(movie1.title);
