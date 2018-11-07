e1a13df3f4089161f2b2585f3c23026981e82d2e
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-07 10:58:19 -0300

    Topic1: Exercise 5

--- 06-React-Native/training-playground/components/CardTwo.js
+++ 06-React-Native/training-playground/components/CardTwo.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const CardTwo = () => {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.textStyle}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin urna sed nibh posuere, non egestas neque rhoncus. Phasellus pharetra vestibulum facilisis. Ut id turpis mi.
-			</Text>
+			<Image
+          style={{width: 400, height: 150}}
+          source={require('../assets/venom.jpg')}
+      />
+      <Image
+          style={{width: 400, height: 150}}
+          source={{uri: 'https://cinepremiere.com.mx/assets/images/noticias/2016/04-abril/Marvel-toma-control-creativo-de-Spider-Man-Homecoming.jpg'}}
+      />
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 100,
+    height: 300,
     width: 400,
-    padding: 10,
     backgroundColor: 'blue'
-  },
-  textStyle: {
-    color: '#fff'
   }
 });
 
