4fb756764f0e60bbd42de827ba8fc222403e54ec
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-07 15:29:58 -0300

    Topic2: Exercise 5

--- 06-React-Native/training-playground/App.js
+++ 06-React-Native/training-playground/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import ExampleThree from './topic2/ExampleThree';
+import ExampleFour from './topic2/ExampleFour';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ExampleThree />
+      <ExampleFour />
     );
   }
 }
