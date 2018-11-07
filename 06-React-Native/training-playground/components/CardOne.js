2aba7859cf4b46edbba2d467c9f4de9e7101b326
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-07 10:45:46 -0300

    Topic1: Exercise 4

--- 06-React-Native/training-playground/components/CardOne.js
+++ 06-React-Native/training-playground/components/CardOne.js
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 const CardOne = () => {
 	return (
 		<View style={styles.container}>
-			<Text>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin urna sed nibh posuere, non egestas neque rhoncus. Phasellus pharetra vestibulum facilisis. Ut id turpis mi. Duis fringilla mauris justo, in finibus lorem euismod eget. Vivamus pellentesque mauris mauris, sit amet accumsan enim auctor eu. Praesent consectetur volutpat vestibulum. Nunc vel sollicitudin nisi, non vulputate mi. 
+			<Text style={styles.baseText}>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin urna sed nibh posuere, non egestas neque rhoncus. Phasellus pharetra vestibulum facilisis. Ut id turpis mi.
+				<Text style={styles.endText}>
+				Duis fringilla mauris justo, in finibus lorem euismod eget. Vivamus pellentesque mauris mauris, sit amet accumsan enim auctor eu. Praesent consectetur volutpat vestibulum. Nunc vel sollicitudin nisi, non vulputate mi. 
+				</Text>
 			</Text>
 		</View>
 	)
@@ -20,6 +23,14 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#000',
     backgroundColor: '#F00'
+  },
+  baseText: {
+  	fontSize: 14
+  },
+  endText: {
+  	fontWeight: 'bold',
+  	fontSize: 20,
+  	color: '#FFF'
   }
 });
 
