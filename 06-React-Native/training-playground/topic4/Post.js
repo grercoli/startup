5c3a3bdec86edf41e96273c6af720df75a99ff42
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-08 20:28:49 -0300

    Topic5: Exercise 4

--- 06-React-Native/training-playground/topic4/Post.js
+++ 06-React-Native/training-playground/topic4/Post.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import TouchOpacity from './TouchOpacity';
 import styles from './styles';
 
 class Post extends React.Component {
 	
+	state = {
+  		user: {}
+  	};
+
+  	fetchUsers() {
+  		const post = this.props.navigation.getParam('post');
+    	return fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      	.then((response) => response.json())
+      	.then((responseJson) => {
+
+	        this.setState({
+	          user: responseJson,
+	          
+	        }, function(){
+
+	        });
+
+      	})
+      	.catch((error) =>{
+        	console.error(error);
+      	});
+  	}
+	componentDidMount(){
+		//Calling a function fetchUsers to dont load to much componentDidMount
+		this.fetchUsers();
+  	}
+
 	render() {
 		const post = this.props.navigation.getParam('post');
 	  	return (
 	    	<View>
 	      		<Text style={styles.postTitle}>{post.title}</Text>
-	      		<Text>by User: {post.userId}</Text>
+	      		<Text>by {this.state.user.name} | Username: {this.state.user.username} | Email: {this.state.user.email}</Text>
 	      		<Text style={styles.postBody}>{post.body}</Text>
 	    	</View>
 	  	);
