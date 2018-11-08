4729e24ee9aedfe09cdbd39be3eb4feab6278385
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-08 11:42:56 -0300

    Topic4: Exercise 7

--- 06-React-Native/training-playground/topic4/Forms3.js
+++ 06-React-Native/training-playground/topic4/Forms3.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import TouchOpacity from './TouchOpacity';
 
 class Forms3 extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Welcome to Form 3</Text>
+        <TouchOpacity type='secondary' name='FINISH' onPress={() => this.props.navigation.navigate('Forms')}/>
       </View>
     );
   }
