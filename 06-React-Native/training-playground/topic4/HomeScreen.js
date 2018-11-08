f7d5bf0a1190688d873c57a5b29e3afaea3b13dc
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-08 11:06:47 -0300

    Topic4: Exercise 4

--- 06-React-Native/training-playground/topic4/HomeScreen.js
+++ 06-React-Native/training-playground/topic4/HomeScreen.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 import DetailsScreen from './DetailsScreen';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <Text>Forms Screen</Text>
       </View>
     );
   }
 }
 
 export default createBottomTabNavigator({
-  Home: { screen: HomeScreen },
-  DetailsScreen: { screen: DetailsScreen },
+  Forms: { screen: HomeScreen },
+  Other: { screen: DetailsScreen },
+},{
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Forms') {
+          iconName = 'ios-list';
+        } else if (routeName === 'Other') {
+          iconName = 'md-information-circle';
+        }
+
+        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#000',
+        inactiveTintColor: 'gray',
+        style: {
+            backgroundColor: '#fff',
+        },
+        indicatorStyle: {
+            backgroundColor: '#000',
+        },
+  	}
 });
